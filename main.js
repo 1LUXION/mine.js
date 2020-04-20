a1d115869c9b085b41179ed81e5aa7b98684d217
sinseiki <ssg2048@gmail.com>
2020-04-20 18:32:07 +0900

    Adjusted the files to make Android App through Adobe Phonegap

--- main.js
+++ main.js
@@ -105,12 +105,12 @@ minefield.init_board(30, 16, 99);
 })();
 function mineWindowUIResize(a, b, c, d) {
     if (!a) {
-        a = Number(prompt('가로 칸 수를 입력하세요(9~110)'));
-        b = Number(prompt('세로 줄 수를 입력하세요(9~50)'));
+        a = Number(prompt('가로 칸 수를 입력하세요(9~30)'));
+        b = Number(prompt('세로 줄 수를 입력하세요(9~16)'));
         d = Number(prompt('칸당 최대 지뢰의 개수를 입력하세요(1~4)'));
         c = Number(prompt('지뢰의 총 개수를 입력하세요'));
-        a = Math.min(Math.max(9, a), 110);
-        b = Math.min(Math.max(9, b), 50);
+        a = Math.min(Math.max(9, a), 30);
+        b = Math.min(Math.max(9, b), 16);
         d = Math.min(Math.max(1, d), 4);
         c = Math.min(Math.max(10, c), ((a * b) - (10 * d)) * d);
     }
